4ab8df29d42731b87342df8270ff3fe1bec30ecc
Sameer <njorogesameer@gmail.com>
2024-12-09 15:20:40 +0300

    Add a form in our users.html file

--- LMS/users/urls.py
+++ LMS/users/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.users_view),
+    path('register/', views.users_view),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
