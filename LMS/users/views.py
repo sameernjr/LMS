9d3c90fe8c0a243bef37b250b851a3bc117a6a53
Sameer <njorogesameer@gmail.com>
2024-12-09 15:03:50 +0300

    Imported UserCreation Form In Views.py IN Users App & Created a variable to contain the form

--- LMS/users/views.py
+++ LMS/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def users_view(request):
-    return render(request, 'users/users.html')
+    form = UserCreationForm()
+    return render(request, 'users/users.html', {'form':form})
