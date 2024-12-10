a398796ab20fc5a9724fb67a159b2745a4c2a95c
Sameer <njorogesameer@gmail.com>
2024-12-10 12:22:48 +0300

    add login and registration to the project

--- LMS/users/views.py
+++ LMS/users/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def users_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("home:index")
     else:
         form = UserCreationForm()
@@ -16,7 +17,7 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            #LOGIN HERE
+            login(request, form.get_user())
             return redirect("home:index")
     else:
         form = AuthenticationForm()
