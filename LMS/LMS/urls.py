2739a1ee08487550cdc1fa9eecea5a76234e2692
Sameer <njorogesameer@gmail.com>
2024-12-16 16:31:00 +0300

    Include USA in Main URls

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('home.urls')),
     path('users/',include('users.urls')),
+    path('usa/',include('usa.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
