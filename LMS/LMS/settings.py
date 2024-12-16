41a348504a279324b64864958847e404f44f499d
Sameer <njorogesameer@gmail.com>
2024-12-16 17:19:21 +0300

    Add Media URL & Media_Root in Settings

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
