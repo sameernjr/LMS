6ba37323c093302d40698da6430083446bb6b8c1
Sameer <njorogesameer@gmail.com>
2024-12-17 12:32:57 +0300

    Add models for each usa university

--- LMS/usa/models.py
+++ LMS/usa/models.py
@@ -8,3 +8,28 @@ class Video(models.Model):
     
     def __str__(self):
         return self.title
+
+class fullsail(models.Model):
+    title = models.CharField(max_length=100)
+    video = models.FileField(upload_to='videos/%y')
+    
+    def __str__(self):
+        return self.title
+class coloradostate(models.Model):
+    title = models.CharField(max_length=100)
+    video = models.FileField(upload_to='videos/%y')
+    
+    def __str__(self):
+        return self.title
+class oregonstate(models.Model):
+    title = models.CharField(max_length=100)
+    video = models.FileField(upload_to='videos/%y')
+    
+    def __str__(self):
+        return self.title
+class saintlouis(models.Model):
+    title = models.CharField(max_length=100)
+    video = models.FileField(upload_to='videos/%y')
+    
+    def __str__(self):
+        return self.title
