73c6f9774b5ab534a4e8416270bf4665e3a76d07
Sameer <njorogesameer@gmail.com>
2024-12-17 12:58:31 +0300

    Add Universities models to views in usa

--- LMS/usa/views.py
+++ LMS/usa/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from .models import Video
+from .models import Video, fullsail, oregonstate, coloradostate, saintlouis
 
 # Create your views here.
 
 def index(request):
     video=Video.objects.all()
     return render(request, 'usa/index.html', {'video': video})
+def fullsail(request):
+    fullsail=fullsail.objects.all()
+    return render(request, 'usa/FullSail/courses.html', {'fullsail': fullsail})
+def coloradostate(request):
+    coloradostate=coloradostate.objects.all()
+    return render(request, 'usa/ColoradoState/courses.html', {'coloradostate': coloradostate})
+def oregonstate(request):
+    oregonstate=oregonstate.objects.all()
+    return render(request, 'usa/OregonState/courses.html', {'oregonstate': oregonstate})
+def saintlouis(request):
+    saintlouis=saintlouis.objects.all()
+    return render(request, 'usa/SaintLouis/courses.html', {'saintlouis': saintlouis})
