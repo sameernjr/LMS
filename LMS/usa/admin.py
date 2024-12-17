042358716f66685105edc10b404f4d24d0eecbc7
Sameer <njorogesameer@gmail.com>
2024-12-17 13:00:42 +0300

    Register all usa universities to admin

--- LMS/usa/admin.py
+++ LMS/usa/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Video
-admin.site.register(Video)
+from .models import fullsail, oregonstate, coloradostate, saintlouis
+admin.site.register(fullsail)
+admin.site.register(oregonstate)
+admin.site.register(coloradostate)
+admin.site.register(saintlouis)
